c2549086a57883dfd9e49b8ff40ae4e5e8419993
Robert Luttrell <robert.luttrell2@gmail.com>
2021-08-19 19:09:16 -0700

    Create generic column headers

--- ExcelWriter/Writer.cs
+++ ExcelWriter/Writer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
+using System.Linq;
 
 namespace ExcelWriter
 {
@@ -20,6 +21,66 @@ namespace ExcelWriter
         }
 
         public void WriteDictToFile()
+        {
+            CreateDocument();
+
+            using (SpreadsheetDocument spreadsheet = SpreadsheetDocument.Open(_outputPath, true))
+            {
+                AddColumnHeaders(spreadsheet);
+
+
+            }
+
+        }
+
+        public Cell FillCellBold(string cellText, string cellIndex, SpreadsheetDocument spreadsheet)
+        {
+            WorkbookPart workbookPart = spreadsheet.WorkbookPart;
+            WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+            SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+
+            //create a new inline string cell
+            Cell cell = new Cell() { CellReference = cellIndex };
+            cell.DataType = CellValues.InlineString;
+
+            //create a run for the bold text
+            Run run1 = new Run();
+            run1.Append(new Text(cellText));
+            //create runproperties and append a "Bold" to them
+            RunProperties run1Properties = new RunProperties();
+            run1Properties.Append(new Bold());
+            //set the first runs RunProperties to the RunProperties containing the bold
+            run1.RunProperties = run1Properties;
+
+            InlineString inlineString = new InlineString();
+            inlineString.Append(run1);
+
+            cell.Append(inlineString);
+
+            return cell;
+        }
+
+        public void AddColumnHeaders(SpreadsheetDocument spreadsheet)
+        {
+            WorkbookPart workbookPart = spreadsheet.WorkbookPart;
+            WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+            SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+            Row row = new Row() { RowIndex = 1 };
+
+            var columnHeaderCells = new List<Cell>();
+            var columnHeaderCellIds = new List<string>() { "A1", "B1", "C1" };
+            var headerTextList = new List<string>() { "header1", "header2", "header3" };
+
+            for (int i = 0; i < columnHeaderCellIds.Count; i++)
+            {
+                Cell cell = FillCellBold(headerTextList[i], columnHeaderCellIds[i], spreadsheet);
+                row.Append(cell);
+            }
+
+            sheetData.Append(row);
+        }
+
+        public void CreateDocument()
         {
             // Create document
             _excelDoc = SpreadsheetDocument.Create(_outputPath, SpreadsheetDocumentType.Workbook);
@@ -40,7 +101,7 @@ namespace ExcelWriter
             {
                 Id = _excelDoc.WorkbookPart.GetIdOfPart(worksheetPart),
                 SheetId = 1,
-                Name = "mySheet"
+                Name = "Worksheet1"
             };
             sheets.Append(sheet);
 
